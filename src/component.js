6c0141cc68d55fa27f87e4d2012430d9d1f3363e
Joel <likedyong@gmail.com>
2016-09-19 21:20:12 +0900

    Improve Component
    - Renamed setRoot to setRootPath for making sure the functionality
    - Refactored loadComponent from create
    - Implemented caching for component definitions and raw htmls
    - Added times function to the promise returned by create method to
      shorcut multiple manipulation of components

--- src/component.js
+++ src/component.js
@@ -1,5 +1,6 @@
 var Component = {
     definitions: {},
+    rawHtmls: {},
     rootPath: 'components',
 };
 
@@ -7,7 +8,7 @@ Component.define = function (name, def) {
     this.definitions[name] = def;
 };
 
-Component.setRoot = function (rootPath) {
+Component.setRootPath = function (rootPath) {
     if (rootPath[rootPath.length - 1] === '/') {
         this.rootPath = rootPath.slice(0, -1);
         return;
@@ -16,39 +17,80 @@ Component.setRoot = function (rootPath) {
     this.rootPath = rootPath;
 };
 
-Component.create = function (name, initParams) {
+Component.loadComponent = function (name) {
+    if (this.definitions[name] && this.rawHtmls[name]) {
+        return Promise.resolve({
+            def: this.definitions[name],
+            rawHtml: this.rawHtmls[name]
+        });
+    }
+
     var self = this;
-
-    return new Promise(function (resolve, reject) {
-        var componentPath = self.rootPath + '/' + name.replace(/\./g, '/');
-
+    var componentPath = self.rootPath + '/' + name.replace(/\./g, '/');
+    return new Promise(function (resolve) {
         $.getScript(componentPath + '.js', function () {
             $.get(componentPath + '.html', function (rawHtml) {
-                var $component = $.extend($(rawHtml), self.definitions[name]);
+                self.rawHtmls[name] = rawHtml;
 
-                if ($component.init) {
-                    $component.init(initParams);
-                }
-
-                if ($component.events) { 
-                    var events = $component.events;
-                    for (var eventType in events) {
-                        var handlers = events[eventType];
-                        for (var key in handlers) {
-                            var handler = handlers[key];
- 
-                            if (key === '.') {
-                                $component.on(eventType, handler.bind($component));
-                            } else {
-                                var selector = key;
-                                $component.on(eventType, selector, handler.bind($component));
-                            }
-                        }
-                    }
-                }
-
-                resolve($component);
+                resolve({
+                    def: self.definitions[name],
+                    rawHtml: self.rawHtmls[name]
+                });
             });
         });
     });
+};
+
+Component.create = function (name) {
+    var self = this;
+
+    function createSingleComponent() {
+        return self.loadComponent(name).then(function (result) {
+            return $.extend($(result.rawHtml), result.def, {
+                update: function (states) {
+                    for (var key in states) {
+                        if (this[key] === undefined) {
+                            continue;
+                        }
+
+                        this[key] = states[key];
+                    }
+
+                    this.render();
+                }
+            });
+        }).then(function ($component) {
+            if ($component.events) {
+                var events = $component.events;
+                for (var eventType in events) {
+                    var handlers = events[eventType];
+                    for (var key in handlers) {
+                        var handler = handlers[key];
+
+                        if (key === '.') {
+                            $component.on(eventType, handler.bind($component));
+                        } else {
+                            var selector = key;
+                            $component.on(eventType, selector, handler.bind($component));
+                        }
+                    }
+                }
+            }
+
+            return $component;
+        });
+    }
+
+    var willCreateComponent = createSingleComponent();
+
+    willCreateComponent.times = function (count) {
+        var promises = [this];
+        for (var i = 0; i < count - 1; ++i) {
+            promises.push(createSingleComponent());
+        }
+
+        return Promise.all(promises);
+    };
+
+    return willCreateComponent;
 };
